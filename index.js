92521743d675573f5d178fca26473aca92d5ab11
cat-in-chair <65989148+CatInChair@users.noreply.github.com>
2021-11-03 13:59:08 +0700

    Edit

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
 const fetch = require("node-fetch");
-const Discord = require('discord.js');
-const FLS = false;
-// const emt = require('emitter');
-const wsr = require('ws');
+const Discord = require("discord.js")
+//const wsr = require('ws');
 
-const { base_url, websocket_base_url, websocket_message_prefix,message_prefix, errorColor, defaultColor } = require("./_cfg.js");
+const { base_url, websocket_base_url, message_websocket_prefix,message_prefix, error_color, default_color } = require("./cfg.json");
 
 function nError(message, exit) {
   if(exit && exit !== false) {
-    console.log(errorColor, prefix + message, defaultColor);
+    console.log(error_color, message_prefix + message, default_color);
     return process.exit();
   } else {
-    return console.log(errorColor, prefix + message, defaultColor);
+    return console.log(error_color, message_prefix + message, default_color);
   }
 }
 function nLog(message) {
-      return console.log(defaultColor, prefix + message, defaultColor);
+      return console.log(default_color, prefix + message, default_color);
 }
 function nWSLog(message) {
-  return console.log(defaultColor, wsprefix + message, defaultColor);
+  return console.log(default_color, message_websocket_prefix + message, default_color);
 }
 function nFLog(message) {
-  return console.log(prefix + message);
+  return console.log(message_prefix + message);
 }
 function nLogWPT(message) {
-  return console.log(defaultColor, prefix + new Date() + message, defaultColor);
+  return console.log(defaultc_color, message_prefix + new Date() + message, default_color);
 }
 
 class APIWrapper {
+
+  /*
+  * @constructor APIWrapper
+  * @param token(String) - MoonBots API token
+  * @param options(Object(
+  *   @param FLS(Boolean) - WTF
+  * )
+  * @return this(APIWrapper) - Returns the instance of this class
+  */
 
   constructor(token, options) {
 
     if(!token) return nError("Вы должны указать токен API!", true);
     this.token = token;
 
-    if(FLS == true) {
-    this.ws = new wsr.WebSocket(wsrr);
-    this.ws.on('error', err => {
-      return nWSLog('Websocket не подключён: ' + err)
-    })
-    this.ws.on('open', function open() {
-      return nWSLog('Websocket подключен');
-    });
-    this.ws.on('close', event => {
-      if (event.wasClean) {
-        nWSLog('Соединение закрыто вами.');
-      } else {
-        nWSLog('Ошибка соединения с Websocket Moonbots.');
-      }
-    });
-    this.ws.on('message', function incoming(message) {
+    /*Пользы в этом коде я не вижу
+    if(options.FLS == true) {
+      this.ws = new wsr.WebSocket(websocket_base_url);
+      this.ws.on('error', err => {
+        return nWSLog('Websocket не подключён: ' + err)
+      })
+      this.ws.on('open', function open() {
+        return nWSLog('Websocket подключен');
+      });
+      this.ws.on('close', event => {
+        if (event.wasClean) {
+          nWSLog('Соединение закрыто вами.');
+        } else {
+          nWSLog('Ошибка соединения с Websocket Moonbots.');
+        }
+      });
+      this.ws.on('message', function incoming(message) {
         nWSLog('Получено сообщение от сервера: ' + message);
       });
-    }
+    }*/
+
     return this;
   }
 
@@ -69,27 +78,29 @@ class APIWrapper {
   }
 
   /*
-  * setAutoPost ASYNC
+  * setAutoPost
   * @param client(Discord.Client(discord.js:12.x-13.x)) - Bot's client object
   * @param interval(Number = 1800000(30min)) - The statistics send interval in ms
+  * @return interval(IntervalObject) - The auto-post interval object
   */
 
-  async setAutoPost(client, interval = 1800000) {
+  setAutoPost(client, interval = 1800000) {
     if(!client) return nError('Вы не указали константу клиента!')
-
     if(interval && interval < 900000) return nError("Отправка статистики возможна не менее одного раза в 15 минут!");
+    
+    return setInterval(this._setStats,interval, client)
+  };
 
-    let s = await (await fetch(`${apr}/api/auth/stats/${client.user.id}`, {
-      method: 'POST',
-      body: JSON.stringify({ servers: client.guilds.cache.size }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': this.token
-      },
-    })).json();
+  /*
+  * _setStats ASYNC PRIVATE
+  * @param client(Discord.Client) - This bot's client object
+  */
+
+  async _setStats(client){
+    let s = await (await fetch(`${base_url}/api/auth/stats/${client.user.id}`, { method: 'POST', body: JSON.stringify({ servers: client.guilds.cache.size }), headers: { 'Content-Type': 'application/json', 'Authorization': this.token}, })).json();
     if(s.success == 'false') return nError('Ошибка отправки статистики! ' + s.error);
-    if(s.success == true) return nLogWPT(' Статистика успешно отправлена на API Moonbots! ' + JSON.stringify(s))
-};
+    if(s.success == 'true') return nLogWPT(' Статистика успешно отправлена на API Moonbots! ' + JSON.stringify(s))
+  }
 };
 
 module.exports = APIWrapper
