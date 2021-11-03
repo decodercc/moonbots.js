02db487e5e568c73cc31c4e81155fa808327660f
cat-in-chair <65989148+CatInChair@users.noreply.github.com>
2021-11-03 13:35:23 +0700

    Delete _cfg.js

--- _cfg.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apr: "https://moonbots.xyz",
-    wsrr: 'ws://localhost:8080',
-    prefix: "[ Moonbots.JS ]: ",
-    wsprefix: "[ Moonbots Websocket ]: ",
-    errorColor: "\x1b[31m",
-    defaultColor: "\x1b[37m"
-}
